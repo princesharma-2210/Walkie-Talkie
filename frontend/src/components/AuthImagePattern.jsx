1f8399b0b5d2237af5813e2d6fee761fcf1a50e0
princesharma-2210 <shaarmprince@gmail.com>
2025-08-01 02:02:05 +0530

    changes in AuthImagePattern

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,27 +1,20 @@
-import React from "react";
-
-const AuthImagePattern = ({ title, subtitle }) => {
-  return (
-    <div className="flex items-center justify-center bg-gray-100 p-12 w-full min-h-screen">
-      <div className="max-w-md text-center">
-        {/* 3x3 Grid */}
-        <div className="grid grid-cols-3 gap-3 mb-8">
-          {[...Array(9)].map((_, i) => (
-            <div
-              key={i}
-              className={`w-16 h-16 rounded-2xl bg-blue-300 ${
-                i % 2 === 0 ? "animate-pulse" : ""
-              }`}
-            />
-          ))}
+const AuthImagePattern = ({ title,subtitle }) => {
+    return (
+      <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+        <div className="max-w-md text-center">
+          <div className="grid grid-cols-3 gap-3 mb-8">
+            {[...Array(9)].map((_, i) => (
+              <div
+                key={i}
+                className={`aspect-square rounded-2xl  bg-blue-100  ${i % 2 === 0 ? "animate-pulse" : ""}`}
+              />
+            ))}
+          </div>
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <p className="text-base-content/60">{subtitle}</p>
         </div>
-
-        {/* Title & Subtitle */}
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
-        <p className="text-gray-600">{subtitle}</p>
       </div>
-    </div>
-  );
-};
-
-export default AuthImagePattern;
+    );
+  };
+  
+  export default AuthImagePattern;
